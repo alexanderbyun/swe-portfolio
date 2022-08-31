808c20a1cc584278e95ad0eb58598d0f93660309
alexanderbyun <byun9110@gmail.com>
2022-08-31 11:37:21 -0400

    intro component

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,4 +1,5 @@
 import "../style/style.css";
+import Profile from "../img/profile.png";
 
 const Intro = () => {
   return (
@@ -7,28 +8,28 @@ const Intro = () => {
       
     <div className="i-left">
         <div className="i-left-wrapper">
-          <h2 className="i-intro">Hello, My name is</h2>
-          <h1 className="i-name">Alexander Byun.</h1>
-          <h2 className="i-intro">But please call me Alex.</h2>
-          <br />
-          <div className="i-title">
-            <div className="i-title-wrapper">
-              <div className="i-title-item">Full Stack Web Developer</div>
-              <div className="i-title-item">Software Engineer</div>
-              <div className="i-title-item">Grillmaster</div>
-              <div className="i-title-item">Sneakerhead</div>
+            <h2 className="i-intro">Hi! My name is</h2>
+            <h1 className="i-name">Alexander Byun,</h1>
+            <h2 className="i-intro">but feel free to call me Alex.</h2>
+            <div className="i-title">
+                <div className="i-title-wrapper">
+                    <div className="i-title-item">Full Stack Web Developer</div>
+                    <div className="i-title-item">Software Engineer</div>
+                    <div className="i-title-item">Grillmaster</div>
+                    {/* <div className="i-title-item">Sneakerhead</div> */}
+                </div>
             </div>
-          </div>
-          <p className="i-desc">
-            I design and develop services for customers of all sizes,
-            specializing in creating stylish, modern websites, web services and
-            online stores.
-          </p>
+            <div className="i-description">
+                <p>
+                    I always strive to leave things better than I found them. With a background in accounting and IT, my passion is in utilizing my varied perspectives to create the digital tools needed to solve problems and streamline workflows.
+                </p>
+            </div>
         </div>
     </div>
 
     <div className="i-right">
-
+        <div className="i-bg"></div>
+        <img src={Profile} alt="Alexander Byun" className="i-img" />
     </div>
 
 
