9db60726ce2a62ff96d72494dd1da2cb1f08f572
alexanderbyun <byun9110@gmail.com>
2022-08-31 03:11:52 -0400

    intro

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -8,14 +8,15 @@ const Intro = () => {
     <div className="i-left">
         <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is</h2>
-          <h1 className="i-name">John Burton</h1>
+          <h1 className="i-name">Alexander Byun.</h1>
+          <h2 className="i-intro">But please call me Alex.</h2>
+          <br />
           <div className="i-title">
             <div className="i-title-wrapper">
-              <div className="i-title-item">Web Developer</div>
-              <div className="i-title-item">UI/UX Designer</div>
-              <div className="i-title-item">Photographer</div>
-              <div className="i-title-item">Writer</div>
-              <div className="i-title-item">Content Creator</div>
+              <div className="i-title-item">Full Stack Web Developer</div>
+              <div className="i-title-item">Software Engineer</div>
+              <div className="i-title-item">Grillmaster</div>
+              <div className="i-title-item">Sneakerhead</div>
             </div>
           </div>
           <p className="i-desc">
